777b72525dc7742546ce9436573b4fa0c94a5137
False_Chicken <jmdevsupport@gmail.com>
2016-06-02 22:47:40 -0400

    - Added comments to stanzaUtils functions.

--- ai_utils/stanzautils.lua
+++ ai_utils/stanzautils.lua
@@ -16,11 +16,17 @@ function stanzaUtils.IsMessageOld(_message)
 	return false;
 end
 
+--[[
+	Returns true if the message is a group chat (MUC) message.
+--]]
 function stanzaUtils.IsGroupChat(_message)
 	if _message.stanza.attr.type == "groupchat" then return true; end
 	return false;
 end
 
+--[[
+	Returns true if the message is a regular chat message (Not MUC).
+--]]
 function stanzaUtils.IsChat(_message)
 	if _message.stanza.attr.type == "chat" then return true; end
 	return false;
